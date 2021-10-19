8dac9800d9be06c0d644448bd861808ed6c6b629
MagsudMirzazada <magsud.mirzezade@gmail.com>
2021-10-19 13:37:16 +0400

    cp

--- src/main/java/Classroom.java
+++ src/main/java/Classroom.java
@@ -1,43 +1,82 @@
-import java.util.List;
+import java.lang.Math;
 
-public class Classroom {
-    private Student student;
-    private List<Student> students;
+public class Classroom{
+    int length;
+    int height;
+    int num_of_tables;
+    private static String course_number;
+    int width;
+    private static String color;
 
-    public Classroom() {
+
+    public Classroom(int h, int l, int w, int num_tables, String c, String col){
+        this.height = h;
+        this.length = l;
+        this.width = w;
+        this.num_of_tables = num_tables;
+        this.course_number = c;
+        this.color = col;
 
     }
 
-    public Classroom(List<Student> students) {
-        this.students = students;
+    public int getNumTables(){
+        return num_of_tables;
     }
 
-    public List<Student> getStudents() {
-        return students;
+    public String getClassNumber(){
+        return course_number;
     }
 
-    public void setStudents(List<Student> students) {
-        this.students = students;
+    public String getClassColor(){
+        return color;
     }
 
-    public boolean addStudent(Student student){
-        getStudents().add(student);
-        return true;
+    public int getHeight(){
+        return height;
     }
 
-    public int studentCount(){
-        return getStudents().size();
+    public int getWidth(){
+        return width;
     }
 
-    public boolean deleteStudent (Student student){
-        getStudents().remove(student);
-        return true;
+    public int getLength(){
+        return length;
     }
 
-    @Override
-    public String toString() {
-        return "CLassroom"  +
-                "students="+  students +
-                '}';
+
+
+
+    public static String getClassNumber(String coursenumber){
+        return "This is class number " + coursenumber;
     }
+
+    public static String getClassColor(String classcolor){
+        return "This class is painted " + classcolor;
+    }
+
+    public static int getMaxNumStudents(int num_tables){
+        int total = 2*num_tables;
+        return total;
+    }
+    public static int area(int l, int w){
+        int total = l*w;
+        return total;
+    }
+
+    public static int volume(int h, int l, int w){
+        int total = h*l*w;
+        return total;
+    }
+
+
+    public static void main(String[] args){
+        Classroom classroom = new Classroom(3, 10, 7, 20, "414","white");
+        System.out.println("Area : " + area(classroom.getLength(), classroom.getWidth()));
+        System.out.println("Volume: " + volume(classroom.getHeight(), classroom.getLength(), classroom.getWidth()));
+        System.out.println("Max number students that can be in classroom: " + getMaxNumStudents(classroom.getNumTables()));
+        System.out.println("Class number: " + getClassNumber(classroom.getClassNumber()));
+        System.out.println("Class color: " + getClassColor(classroom.getClassColor()));
+
+    }
+
 }
--- src/test/java/ClassroomTest.java
+++ src/test/java/ClassroomTest.java
@@ -1,44 +1,24 @@
-import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ClassroomTest {
-
-    Student tom = new Student("Tom", "Johnson", "tom1@gmail.com", "IT2021");
-    Student anna = new Student("Anna", "Bell", "anna1@gmail.com", "IT2021");
-    Student layla = new Student("Layla", "Smith", "layla@gmail.com", "IT2021");
-    Student david = new Student("David", "Brown", "dev@gmail.com", "IT2021");
-
-    List<Student> students = new ArrayList<>(List.of(tom, anna, layla));
-    Classroom it2021 = new Classroom(students);
-
-
-    @DisplayName("Test for addStudent() method")
     @Test
-    void addStudent() {
-        Student david = new Student("David", "Brown", "dev@gmail.com", "IT2021");
-        Assertions.assertTrue(it2021.addStudent(david));
+    void vol_test(){
+        assertEquals(210, Classroom.volume(3, 10, 7));
     }
 
 
-    @DisplayName("Test for deleteStudent() method")
     @Test
-    void deleteStudent() {
-        Student david = new Student("David", "Brown", "dev@gmail.com", "IT2021");
-        Assertions.assertTrue(it2021.deleteStudent(david));
-
+    void area_test(){
+        assertEquals(70, Classroom.area(10, 7));
     }
 
-    @DisplayName("Test for studentCount() method")
     @Test
-    void studentCount() {
-        Assertions.assertEquals(3, it2021.studentCount());
-    }
+    void  max_num_stu_test() { assertEquals(40, Classroom.getMaxNumStudents(20));}
 
+    @Test
+    void test_class_number() { assertEquals("This is class number 414", Classroom.getClassNumber("414"));}
+
+    @Test
+    void test_class_color() { assertEquals("This class is painted white", Classroom.getClassColor("white"));}
 }
